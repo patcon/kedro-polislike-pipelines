43d2fb07c44099c82a7fc5cec6f3fa972b96ebc5
Patrick Connolly <patrick.c.connolly@gmail.com>
2025-07-27 18:58:26 -0500

    Added save_load_index to CustomCSVDataset to reduce boilerplate in catalog.yml.

--- src/kedro_polis_classic/datasets/pandas/csv_dataset.py
+++ src/kedro_polis_classic/datasets/pandas/csv_dataset.py
@@ -1,7 +1,41 @@
 from kedro_datasets.pandas.csv_dataset import CSVDataset, TablePreview
+from copy import deepcopy
 import pandas as pd
 
 class CustomCSVDataset(CSVDataset):
+    def __init__(self, *args, **kwargs):
+        """
+        A drop-in replacement for `kedro_datasets.pandas.CSVDataset` with optional preview
+        enhancements and shorthand index configuration.
+
+        This subclass supports two custom `metadata` flags:
+
+        - `metadata.save_load_index`: When `True`, injects default index behavior
+          equivalent to setting the following in the catalog entry:
+              load_args:
+                index_col: 0
+              save_args:
+                index: true
+
+        - `metadata.index_in_preview`: When `True`, includes the index in kedro-viz
+          previews (unless it's a default RangeIndex). The index column is renamed
+          to `"__index__"` if unnamed.
+
+        All other behavior is identical to the upstream `CSVDataset`. For full details,
+        see the `kedro_datasets.pandas.CSVDataset` documentation.
+        """
+        metadata = deepcopy(kwargs.get("metadata", {}))
+        load_args = deepcopy(kwargs.get("load_args", {})) or {}
+        save_args = deepcopy(kwargs.get("save_args", {})) or {}
+
+        if metadata.get("save_load_index", False):
+            load_args.setdefault("index_col", 0)
+            save_args.setdefault("index", True)
+            kwargs["load_args"] = load_args
+            kwargs["save_args"] = save_args
+
+        super().__init__(*args, **kwargs)
+
     def preview(self, nrows: int = 10) -> TablePreview:
         """
         Generate a preview of the dataset with a specified number of rows.
