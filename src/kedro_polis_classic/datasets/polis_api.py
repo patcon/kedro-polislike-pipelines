2af13e2b1e440093190ba7b2046a6a64002b90f6
Patrick Connolly <patrick.c.connolly@gmail.com>
2025-07-28 01:54:44 -0500

    Removed unnecessary typing from polis_api dataset.

--- src/kedro_polis_classic/datasets/polis_api.py
+++ src/kedro_polis_classic/datasets/polis_api.py
@@ -1,12 +1,11 @@
 import pandas as pd
 from kedro.io import AbstractDataset
-from typing import Dict
 
 class PolisAPIDataset(AbstractDataset):
     def __init__(self, report_id: str):
         self.report_id = report_id
 
-    def load(self) -> Dict[str, pd.DataFrame]:
+    def load(self) -> dict[str, pd.DataFrame]:
         base = f"https://pol.is/api/v3/reportExport/{self.report_id}"
         comments_url = f"{base}/comments.csv"
         votes_url = f"{base}/votes.csv"
@@ -16,7 +15,7 @@ class PolisAPIDataset(AbstractDataset):
 
         return {"comments": comments, "votes": votes}
 
-    def save(self, data: Dict[str, pd.DataFrame]) -> None:
+    def save(self, data: dict[str, pd.DataFrame]) -> None:
         raise NotImplementedError("Saving to Polis API is not supported.")
 
     def _describe(self) -> dict:
