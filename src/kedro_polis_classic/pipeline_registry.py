01921abdedaf5b31ff5decc4869a27b972423b6e
Patrick Connolly <patrick.c.connolly@gmail.com>
2025-08-31 01:27:27 -0400

    Cleaner pipeline registry.

--- src/kedro_polis_classic/pipeline_registry.py
+++ src/kedro_polis_classic/pipeline_registry.py
@@ -4,10 +4,23 @@ from .pipelines.experimental import pipeline as experiment_pipeline
 
 
 def register_pipelines() -> dict[str, Pipeline]:
-    return {
+    # List of experimental pipeline names
+    experimental_pipeline_names = [
+        "polis_classic",
+        "mean_pacmap_kmeans",
+        "mean_localmap_kmeans",
+        "knn_pacmap_kmeans",
+        "knn_localmap_kmeans",
+    ]
+
+    # Create base pipelines
+    pipelines = {
         "polis": polis_pipeline.create_pipeline(),
-        "polis_classic": experiment_pipeline.create_pipeline("polis_classic"),
-        "knn_pacmap_kmeans": experiment_pipeline.create_pipeline("knn_pacmap_kmeans"),
-        "knn_localmap_kmeans": experiment_pipeline.create_pipeline("knn_localmap_kmeans"),
         "__default__": polis_pipeline.create_pipeline(),
     }
+
+    # Add experimental pipelines using iteration
+    for name in experimental_pipeline_names:
+        pipelines[name] = experiment_pipeline.create_pipeline(name)
+
+    return pipelines
